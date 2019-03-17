f2006a974ab5e4a662209aa72aaf7f2dfe3f8349
gramps <richmoyer@gmail.com>
2019-03-17 11:00:18 -0500

    Add files via upload

--- init.el
+++ init.el
@@ -26,13 +26,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (doom-nord)))
+ '(custom-enabled-themes (quote (doom-one)))
  '(custom-safe-themes
    (quote
-    ("7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" default)))
+    ("d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" default)))
  '(package-selected-packages
    (quote
-    (avy smex ido-vertical-mode linum-relative doom-modeline doom-nord doom-themes which-key use-package))))
+    (paredit-everywhere slime clojure-mode paredit company-irony sly jedi neotree avy smex ido-vertical-mode linum-relative doom-modeline doom-city-lights doom-themes which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
