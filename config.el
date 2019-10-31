006acd8183604cfbd11a06701fe79c793e02bc85
gramps <richmoyer@gmail.com>
2019-10-31 12:13:52 -0500

    doom config.el

--- config.el
+++ config.el
@@ -1,88 +1,30 @@
-(defvar my-term-shell "/bin/bash")
-(defadvice ansi-term (before force-bash)
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-(global-set-key (kbd "<M-return>") 'ansi-term)
+;; Place your private configuration here
+(require 'company)
+(setq company-idle-delay 0.2
+      company-minimum-prefix-length 3)
 
-(setq make-backup-file nil)
+(setq display-line-numbers-type 'relative)
 
-(setq auto-save-default nil)
+(setq doom-font (font-spec :family "Hack" :size 15))
 
-(defalias 'yes-or-no-p 'y-or-n-p)
+(require 'doom-themes)
 
-(setq scroll-conservatively 100)
+(load-theme 'doom-one t)
 
-(setq ring-bell-function 'ignore)
+(setq doom-modeline-major-mode-icon t)
 
-(when window-system (global-hl-line-mode t))
+(setq winner-mode 1)
 
-(when window-system (global-prettify-symbols-mode t))
+(setq +ivy-buffer-icons t)
 
-(setq inhibit-startup-message t)
+(doom-themes-org-config)
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(setq doom-themes-treemacs-theme "doom-colors")
+(doom-themes-treemacs-config)
 
-(use-package which-key
-  :ensure t
-  :init (which-key-mode))
+(doom-themes-org-config)
 
-(use-package doom-modeline
-  :ensure t
-  :hook (after-init . doom-modeline-mode))
-
-(use-package linum-relative
-  :ensure t
-  :init (linum-relative-on))
-
-(setq ido-enable-flex-matching nil)
-(setq ido-create-new-buffer 'always)
-(setq iso-everywhere t)
-(ido-mode 1)
-
-(use-package ido-vertical-mode
-  :ensure t
-  :init
-  (ido-vertical-mode 1))
-(setq ido-vertical-define-keys 'C-n-and-C-p-only)
-
-(use-package smex
-  :ensure t
-  :init (smex-initialize)
-  :bind
-  ("M-x" . smex))
-
-(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
-
-(global-set-key (kbd "C-x b") 'ibuffer)
-
-(use-package avy
-  :ensure t
-  :bind
-  ("M-s" . avy-goto-char-2))
-
-(defun config-visit ()
-  (interactive)
-  (find-file "~/.emacs.d/config.org"))
-(global-set-key (kbd "C-c e") 'config-visit)
-
-(defun config-reload ()
-  (interactive)
-  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-(global-set-key (kbd "C-c r") 'config-reload)
-
-(defun split-and-follow-horizontally ()
-  (interactive)
-  (split-window-below)
-  (balance-windows)
-  (other-window 1))
-(global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
-
-(defun split-and-follow-vertically ()
-  (interactive)
-  (split-window-right)
-  (balance-windows)
-  (other-window 1))
-(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
+(add-to-list 'default-frame-alist '(height . 68))
+(add-to-list 'default-frame-alist '(width . 239))
