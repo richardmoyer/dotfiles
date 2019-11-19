2c1b841adbd9e8ea10eb1937363949b9b1f6be7e
gramps <richmoyer@gmail.com>
2019-11-19 06:50:35 -0600

    add custom scratch buffer message

--- config.el
+++ config.el
@@ -1,7 +1,11 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+(setq user-full-name "Richard Moyer"
+      user-mail-address "richmoyer@gmail.com")
+
 (require 'company)
+
 (setq company-idle-delay 0.2
       company-minimum-prefix-length 3)
 
@@ -19,12 +23,24 @@
 
 (setq +ivy-buffer-icons t)
 
-(doom-themes-org-config)
+(setq racer-rust-src-path "/home/richm/.rust/src/")
 
-(setq doom-themes-treemacs-theme "doom-colors")
+(setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
 (doom-themes-treemacs-config)
 
+;; Corrects (and improves) org-mode's native fontification.
 (doom-themes-org-config)
 
-(add-to-list 'default-frame-alist '(height . 68))
-(add-to-list 'default-frame-alist '(width . 239))
+(add-to-list 'default-frame-alist '(height . 54))
+(add-to-list 'default-frame-alist '(width . 206))
+
+(setq initial-scratch-message (expand-file-name "~/.doom.d/scratch-message.txt"))
+
+(setq initial-scratch-message (with-temp-buffer
+                                (insert-file-contents
+                                 (expand-file-name "scratch-message.txt"
+                                                   user-emacs-directory))
+                                (buffer-string)))
+(with-current-buffer
+    (get-buffer "*scratch*")
+  (lisp-interaction-mode))
